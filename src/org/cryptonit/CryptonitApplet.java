cb301996fb82ac7738eb924b8be9e759f0f2357c
Mathias Brossard <mathias@brossard.org>
2016-11-16 22:30:51 -0800

    Superfluous import

--- src/org/cryptonit/CryptonitApplet.java
+++ src/org/cryptonit/CryptonitApplet.java
@@ -1,7 +1,6 @@
 package org.cryptonit;
 
 import javacard.framework.APDU;
-import javacard.framework.APDUException;
 import javacard.framework.Applet;
 import javacard.framework.ISO7816;
 import javacard.framework.ISOException;
