548e94455ddd384cc110f0372ed1d9d8b2ad509a
Mathias Brossard <mathias@brossard.org>
2016-11-22 01:15:33 -0800

    Add array of booleans to maintain state

--- src/org/cryptonit/IOBuffer.java
+++ src/org/cryptonit/IOBuffer.java
@@ -6,8 +6,10 @@ public class IOBuffer {
     private FileIndex index = null;
     private byte [] buffer = null;
 
+    private boolean [] bools = null;
     public IOBuffer(FileIndex index) {
         this.index = index;
+        this.bools = JCSystem.makeTransientBooleanArray((short) 2, JCSystem.CLEAR_ON_DESELECT);
         this.buffer = JCSystem.makeTransientByteArray((short)256, JCSystem.CLEAR_ON_DESELECT);
     }
 }
