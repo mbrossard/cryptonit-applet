236ddee90c49f6efcdef5030ad8185aa0d4ec189
Mathias Brossard <mathias@brossard.org>
2016-11-22 23:59:48 -0800

    Additional array for integer metadata values

--- src/org/cryptonit/IOBuffer.java
+++ src/org/cryptonit/IOBuffer.java
@@ -6,6 +6,7 @@ public class IOBuffer {
     private FileIndex index = null;
     private byte [] buffer = null;
 
+    private short [] shorts = null;
     private boolean [] bools = null;
     final private byte isLOADED = 0x0;
     final private byte isFILE   = 0x1;
@@ -14,5 +15,6 @@ public class IOBuffer {
         this.index = index;
         this.bools = JCSystem.makeTransientBooleanArray((short) 2, JCSystem.CLEAR_ON_DESELECT);
         this.buffer = JCSystem.makeTransientByteArray((short)256, JCSystem.CLEAR_ON_DESELECT);
+        this.shorts = JCSystem.makeTransientShortArray((short)3, JCSystem.CLEAR_ON_DESELECT);
     }
 }
