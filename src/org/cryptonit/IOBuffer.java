0728dd688d8d6fccdf88c06ab232c0e7321cb766
Mathias Brossard <mathias@brossard.org>
2016-11-28 16:12:31 -0800

    Fix white space in IOBuffer

--- src/org/cryptonit/IOBuffer.java
+++ src/org/cryptonit/IOBuffer.java
@@ -6,41 +6,40 @@ import javacard.framework.ISOException;
 import javacard.framework.JCSystem;
 import javacard.framework.Util;
 
-
 public class IOBuffer {
     private FileIndex index = null;
-    private byte [] buffer = null;
+    private byte[] buffer = null;
 
-    private short [] shorts = null;
-    final private byte SIZE = 0x0;
-    final private byte PATH = 0x1;
+    private short[] shorts = null;
+    final private byte SIZE   = 0x0;
+    final private byte PATH   = 0x1;
     final private byte OFFSET = 0x2;
 
-    private boolean [] bools = null;
+    private boolean[] bools = null;
     final private byte isLOADED = 0x0;
     final private byte isFILE   = 0x1;
 
     public IOBuffer(FileIndex index) {
         this.index = index;
         this.bools = JCSystem.makeTransientBooleanArray((short) 2, JCSystem.CLEAR_ON_DESELECT);
-        this.buffer = JCSystem.makeTransientByteArray((short)256, JCSystem.CLEAR_ON_DESELECT);
-        this.shorts = JCSystem.makeTransientShortArray((short)3, JCSystem.CLEAR_ON_DESELECT);
+        this.buffer = JCSystem.makeTransientByteArray((short) 256, JCSystem.CLEAR_ON_DESELECT);
+        this.shorts = JCSystem.makeTransientShortArray((short) 3, JCSystem.CLEAR_ON_DESELECT);
     }
 
     public void sendBuffer(byte[] buf, short length, APDU apdu) {
         short le = apdu.setOutgoing(), r = 0;
 
-        if(le == 0) {
+        if (le == 0) {
             le = (short) (APDU.getOutBlockSize() - 2);
         }
 
-        if(le > length) {
+        if (le > length) {
             le = length;
         }
 
-        if(le < length) {
+        if (le < length) {
             r = (short) (length - le);
-            if(r > (short) this.buffer.length) {
+            if (r > (short) this.buffer.length) {
                 ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
             }
             this.bools[isLOADED] = true;
@@ -51,7 +50,7 @@ public class IOBuffer {
         apdu.setOutgoingLength(le);
         apdu.sendBytesLong(buf, (short) 0, le);
 
-        if(r > 0) {
+        if (r > 0) {
             if (r >= (short) (APDU.getOutBlockSize() - 2)) {
                 r = 0;
             }
@@ -61,7 +60,7 @@ public class IOBuffer {
             this.bools[isLOADED] = false;
         }
     }
- 
+
     public void sendFile(short id, APDU apdu, short offset) {
         byte[] d = index.entries[id].content;
         if (d == null) {
@@ -69,7 +68,7 @@ public class IOBuffer {
         }
 
         short le = apdu.setOutgoing(), r = 0;
-        if(le == 0) {
+        if (le == 0) {
             le = (short) (APDU.getOutBlockSize() - 2);
         }
 
@@ -77,12 +76,12 @@ public class IOBuffer {
             le = (short) (d.length - offset);
         }
 
-        if((short) (le + offset) < (short) d.length) {
+        if ((short) (le + offset) < (short) d.length) {
             r = (short) (d.length - (le + offset));
             this.bools[isLOADED] = true;
             this.bools[isFILE]   = true;
             this.shorts[OFFSET]  = (short) (le + offset);
-            this.shorts[PATH]  = id;
+            this.shorts[PATH]    = id;
         }
 
         apdu.setOutgoingLength(le);
@@ -99,11 +98,11 @@ public class IOBuffer {
     }
 
     public void getResponse(APDU apdu) {
-        if(!this.bools[isLOADED]) {
+        if (!this.bools[isLOADED]) {
             ISOException.throwIt(ISO7816.SW_UNKNOWN);
         }
 
-        if(this.bools[isFILE]) {
+        if (this.bools[isFILE]) {
             sendFile(this.shorts[PATH], apdu, this.shorts[OFFSET]);
         } else {
             sendBuffer(this.buffer, this.shorts[SIZE], apdu);
