6f651363c26371b4cc3aa5fab3c1244e8e2b5fd4
Mathias Brossard <mathias@brossard.org>
2016-12-07 00:28:31 -0800

    Initialize signature operation

--- src/org/cryptonit/CryptonitApplet.java
+++ src/org/cryptonit/CryptonitApplet.java
@@ -505,6 +505,7 @@ public class CryptonitApplet extends Applet implements ExtendedLength {
                 Util.arrayCopy(new byte[]{
                     (byte) 0x7C, (byte) 0x43, (byte) 0x82, (byte) 0x41
                 }, (short) 0, signature, (short) 0, (short) 4);
+                ec_signature.init(keys[id], Signature.MODE_SIGN);
             }
             io.sendBuffer(signature, l, apdu);
         }
