f6915abf5ebf1bad7206147069e1243e84822642
Mathias Brossard <mathias@brossard.org>
2016-12-07 00:29:51 -0800

    Throw exception when operation not supported

--- src/org/cryptonit/CryptonitApplet.java
+++ src/org/cryptonit/CryptonitApplet.java
@@ -499,7 +499,9 @@ public class CryptonitApplet extends Applet implements ExtendedLength {
                     ISOException.throwIt(ISO7816.SW_UNKNOWN);
                 }
             } else if (keys[id].getType() == KeyPair.ALG_EC_FP) {
-                ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+                if (ec_signature == null) {
+                    ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+                }
                 l = (short) 69;
                 signature = new byte[l];
                 Util.arrayCopy(new byte[]{
