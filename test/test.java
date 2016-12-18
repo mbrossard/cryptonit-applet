4da849f38aa07cdc3023f407f16ff8175eb28793
Mathias Brossard <mathias@brossard.org>
2016-12-18 02:42:43 -0800

    Generate RSA key test

--- test/test.java
+++ test/test.java
@@ -40,5 +40,12 @@ class test {
         })).getBytes()));
         System.out.println(response.toString());
         System.out.println(toHex(response.getData()));
+
+        System.out.println("Generate 2048 bit RSA key (9A)");
+        response = new ResponseAPDU(simulator.transmitCommand((new CommandAPDU(0x00, 0x47, 0x00, 0x9A, new byte []{
+                (byte) 0xAC, (byte) 0x03, (byte) 0x80, (byte) 0x01, (byte) 0x07
+        }, 0x200)).getBytes()));
+        System.out.println(response.toString());
+        System.out.println(toHex(response.getData()));
     }
 }
