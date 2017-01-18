d215f5d9275aec3e0cc778256e5a090231ad9515
Mathias Brossard <mathias@brossard.org>
2017-01-18 00:22:10 -0800

    New test file

---
+++ test/piv.java
@@ -0,0 +1,11 @@
+import com.licel.jcardsim.base.Simulator;
+
+/**
+ * @author Mathias Brossard
+ */
+
+class piv {
+    public static void main(String[] args) {
+        Simulator simulator = new Simulator();
+    }
+}
