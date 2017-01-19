830ac97f8dafa18d2b9188fafda1561278755a03
Mathias Brossard <mathias@brossard.org>
2017-01-19 06:38:20 -0800

    Convert select applet call

--- test/piv.java
+++ test/piv.java
@@ -27,6 +27,7 @@ class piv {
     }
 
     public static void main(String[] args) {
+        ResponseAPDU response;
         Simulator simulator = new Simulator();
         byte[] arg;
         byte[] appletAIDBytes = new byte[]{
@@ -39,11 +40,9 @@ class piv {
 
         simulator.installApplet(appletAID, CryptonitApplet.class);
         System.out.println("Select Applet");
-        ResponseAPDU response = new ResponseAPDU(simulator.transmitCommand((new CommandAPDU(0x00, 0xA4, 0x04, 0x00, new byte[]{
+        response = sendAPDU(simulator, new CommandAPDU(0x00, 0xA4, 0x04, 0x00, new byte[]{
             (byte) 0xA0, (byte) 0x00, (byte) 0x00, (byte) 0x03, (byte) 0x08
-        })).getBytes()));
-        System.out.println(response.toString());
-        System.out.println(toHex(response.getData()));
+        }));
 
         System.out.println("Management key authentication (part 1)");
         response = new ResponseAPDU(simulator.transmitCommand((new CommandAPDU(0x00, 0x87, 0x03, 0x9B,  new byte []{
