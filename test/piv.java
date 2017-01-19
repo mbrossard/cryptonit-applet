f72a224d50bc3588b986ad4afd5b4cf807e0b35d
Mathias Brossard <mathias@brossard.org>
2017-01-19 05:15:59 -0800

    Load AID

--- test/piv.java
+++ test/piv.java
@@ -1,4 +1,5 @@
 import com.licel.jcardsim.base.Simulator;
+import javacard.framework.AID;
 
 /**
  * @author Mathias Brossard
@@ -7,5 +8,13 @@ import com.licel.jcardsim.base.Simulator;
 class piv {
     public static void main(String[] args) {
         Simulator simulator = new Simulator();
+        byte[] arg;
+        byte[] appletAIDBytes = new byte[]{
+            (byte) 0xA0, (byte) 0x00, (byte) 0x00, (byte) 0x03,
+            (byte) 0x08, (byte) 0x00, (byte) 0x00, (byte) 0x10,
+            (byte) 0x00
+        };
+        short sw, le;
+        AID appletAID = new AID(appletAIDBytes, (short) 0, (byte) appletAIDBytes.length);
     }
 }
