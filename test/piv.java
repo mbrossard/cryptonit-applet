31fc58fb3cfc34fc256cac740be2d961a4fcb723
Mathias Brossard <mathias@brossard.org>
2017-01-22 23:05:24 -0800

    Constant byte array prefix for certificate import

--- test/piv.java
+++ test/piv.java
@@ -217,5 +217,10 @@ class piv {
         } catch (Exception ex) {
             ex.printStackTrace(System.out);
         }
+
+        byte[] import_prefix = new byte[]{
+            (byte) 0x5C, (byte) 0x03, (byte) 0x5F, (byte) 0xC1, (byte) 0x05,
+            (byte) 0x53, (byte) 0x82 
+        };
     }
 }
