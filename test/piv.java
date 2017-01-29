2cc9b14bb5889ac2c6180cccbe81cd59801514cd
Mathias Brossard <mathias@brossard.org>
2017-01-29 00:00:10 -0800

    Extract ECDSA signature value

--- test/piv.java
+++ test/piv.java
@@ -336,6 +336,9 @@ class piv {
         Util.arrayCopy(sig_prefix, (short) 0, sig_request, (short) 0, (short) sig_prefix.length);
         Util.arrayCopy(digest, (short) 0, sig_request, (short) (sig_prefix.length), (short) (digest.length));
         response = sendAPDU(simulator, new CommandAPDU(0x00, 0x87, 0x07, 0x9C, sig_request));
+
+        arg = response.getData();
+        sig = Arrays.copyOfRange(arg, 4, arg.length);
         System.out.println("Set Card Capabilities Container");
         response = sendAPDU(simulator, new CommandAPDU(0x00, 0xDB, 0x3F, 0xFF, new byte[]{
             (byte) 0x5C, (byte) 0x03, (byte) 0x5F, (byte) 0xC1, (byte) 0x07,
