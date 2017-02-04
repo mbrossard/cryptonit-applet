98fd8712544e6714eee16c13ed3ad603b43896c1
Mathias Brossard <mathias@brossard.org>
2017-02-04 02:33:55 -0800

    Adding decodeLength static method

--- src/org/cryptonit/BERTLV.java
+++ src/org/cryptonit/BERTLV.java
@@ -1,5 +1,24 @@
 package org.cryptonit;
 
-public class BERTLV {
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.Util;
 
+public class BERTLV {
+    public static short decodeLength(byte[] buf, short offset) {
+        byte b = buf[offset];
+        short s = buf[offset];
+        if ((b & (byte) 0x80) != 0) {
+            offset += 1;
+
+            if (b == (byte) 0x81) {
+                s = (short) (0x00FF & buf[offset]);
+            } else if (b == (byte) 0x82) {
+                s = Util.getShort(buf, offset);
+            } else {
+                ISOException.throwIt(ISO7816.SW_UNKNOWN);
+            }
+        }
+        return s;
+    }
 }
