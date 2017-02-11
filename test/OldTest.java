8411610980617801e491eb5f72806e3e270ea757
Mathias Brossard <mathias@brossard.org>
2017-02-10 22:04:06 -0800

    Fix name collision

--- test/OldTest.java
+++ test/OldTest.java
@@ -13,14 +13,14 @@ import org.junit.Test;
 /**
  * @author Mathias Brossard
  */
-public class Test {
+public class OldTest {
     private static String toHex(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < bytes.length; i++) {
             if((i != 0) && ((i % 32) == 0)) {
                 sb.append("\n");
             }
-            sb.append(String.format("%02X ", bytes[i]));
+            sb.append(String.format("%02x ", bytes[i]));
         }
         return sb.toString();
     }
@@ -29,7 +29,7 @@ public class Test {
         return toHex(arg.getBytes());
     }
 
-    public Test() {
+    public OldTest() {
     }
 
     @Test
