e62c5d867d4ae80381fcc94b5b07342e23120f1d
Mathias Brossard <mathias@brossard.org>
2017-02-13 23:31:48 -0800

    Factorize EC support test

--- src/org/cryptonit/CryptonitApplet.java
+++ src/org/cryptonit/CryptonitApplet.java
@@ -404,15 +404,9 @@ public class CryptonitApplet extends Applet implements ExtendedLength {
                 doGenRSA(apdu, buf[ISO7816.OFFSET_P2]);
                 break;
             case 0x11:
-                if (ec_signature == null) {
-                    ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
-                }
                 doGenEC(apdu, buf[ISO7816.OFFSET_P2], (short) 256);
                 break;
             case 0x14:
-                if (ec_signature == null) {
-                    ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
-                }
                 doGenEC(apdu, buf[ISO7816.OFFSET_P2], (short) 384);
                 break;
             default:
@@ -482,6 +476,11 @@ public class CryptonitApplet extends Applet implements ExtendedLength {
     private void doGenEC(APDU apdu, byte keyRef, short size) {
         KeyPair kp = null;
         byte id = keyMapping(keyRef);
+
+        if (ec_signature == null) {
+            ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+        }
+
         try {
             kp = new KeyPair(KeyPair.ALG_EC_FP, size);
         } catch (CryptoException e) {
